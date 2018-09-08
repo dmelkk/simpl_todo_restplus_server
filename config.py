759aab8f91db2272caabc840506c520469ed3371
dmelk <dmelkk@gmail.com>
2018-09-08 16:23:40 +0300

    update gitignore and configurations

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+
+
+class Config(object):
+    SECRET_KEY = os.getenv('SECRET_KEY') or 'hard to guess'
+
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or \
+            'sqlite:///' + os.path.join(PROJECT_ROOT, 'app.db')
+
+    DEBUG = False
+
+
+class ProductionConfig(Config):
+    pass
+
+
+class DevelopmentConfig(Config):
+    Debug = True
+
+
+class TestingConfig(Config):
+    Testing = True
